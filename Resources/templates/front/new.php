746cabd12788ad6096a9cdcb9921f2c8ca0c6ce2
AqeelUrRahman <42336786+AqeelurRahman@users.noreply.github.com>
2018-12-03 11:19:50 +0500

    Initial commit

---
+++ Resources/templates/front/new.php
@@ -0,0 +1,117 @@
+
+  <!DOCTYPE html>
+  <!--[if lt IE 7 ]>
+  <html class="ie ie6" lang="en"> <![endif]-->
+  <!--[if IE 7 ]>
+  <html class="ie ie7" lang="en"> <![endif]-->
+  <!--[if IE 8 ]>
+  <html class="ie ie8" lang="en"> <![endif]-->
+  <!--[if (gte IE 9)|!(IE)]><!-->
+  <html lang="en"> <!--<![endif]-->
+  <head>
+    <meta charset="utf-8">
+    <title>ThemeSuite | Automotive</title>
+
+    <!-- Mobile Specific -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+
+    <!-- CSS Style -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/jquery-ui-1.10.4.custom.css" rel="stylesheet"/>
+    <link href="css/dd.css" rel="stylesheet"/>
+
+    <link href="css/animate.min.css" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+
+    <!-- Used Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,700" rel="stylesheet" type="text/css">
+
+    <!-- JavaScript -->
+    <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js/jquery.cookie.js"></script>
+    <script type="text/javascript" src="js/jquery.dd.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    <script type="text/javascript" src="js/styleswitcher.js"></script>
+    <script type="text/javascript" src="js/masonry.min.js"></script>
+  </head>
+
+  <body class='automotive-wp'>
+
+  <div id="switcher">
+    <div class="center">
+      <div class="logo">
+        <a href="https://themeforest.net/user/themesuite/portfolio" target="_blank" title="ThemeSuite">
+          <img src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+               alt="ThemeSuite" class="logo_image"/>
+        </a>
+      </div>
+
+      <ul>
+        <li id="theme_list">
+          <a id="theme_select" href="#" data-id="Automotive-WP">
+            <i class='fa fa-wordpress dropdown-icon'></i>Automotive          </a>
+          <ul>
+            <li class="button_a"><a href="#" rel="https://demo.themesuite.com/hair-salon/,https://themeforest.net/item/hair-salon-html-template-for-barber-shops-beauty-salons/20768747?ref=themesuite,Hair-Salon" data-id="Hair-Salon" data-icon="html5" data-label="Hair-Salon"><i class="fa fa-html5 dropdown-icon"></i> Hair-Salon  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/hair-salon.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/lawyer/,https://themeforest.net/item/lawyer-legal-html-template-for-attorneys-and-law-firms/20721838?ref=themesuite,Lawyer" data-id="Lawyer" data-icon="html5" data-label="Lawyer"><i class="fa fa-html5 dropdown-icon"></i> Lawyer  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/lawyer.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/funeral/,https://themeforest.net/item/funeral-home-funeral-services-church-html-template/20633868?ref=themesuite,Funeral" data-id="Funeral" data-icon="html5" data-label="Funeral"><i class="fa fa-html5 dropdown-icon"></i> Funeral  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/funeral.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/landscaping-wp/welcome/,https://themeforest.net/item/landscaping-landscape-construction-wordpress-theme/19580155?ref=themesuite,Landscaping" data-id="Landscaping-WP" data-icon="wordpress" data-label="Landscaping"><i class="fa fa-wordpress dropdown-icon"></i> Landscaping  <span style="background:#0074a2">WordPress</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/landscaping-wp.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/landscaping/,https://themeforest.net/item/landscaping-lawn-garden-landscape-construction-snow-removal-html-template/18797050?ref=themesuite,Landscaping" data-id="Landscaping" data-icon="html5" data-label="Landscaping"><i class="fa fa-html5 dropdown-icon"></i> Landscaping  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/landscaping.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/automotive-wp/,https://themeforest.net/item/automotive-car-dealership-business-wordpress-theme/9210971?ref=themesuite,Automotive" data-id="Automotive-WP" data-icon="wordpress" data-label="Automotive"><i class="fa fa-wordpress dropdown-icon"></i> Automotive  <span style="background:#0074a2">WordPress</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/automotive-wp.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/automotive/,https://themeforest.net/item/automotive-car-dealership-business-html-template/8030193?ref=themesuite,Automotive" data-id="Automotive" data-icon="html5" data-label="Automotive"><i class="fa fa-html5 dropdown-icon"></i> Automotive  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/automotive.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/flight-wp/,https://themeforest.net/item/flight-responsive-fullscreen-background-theme/3917050?ref=themesuite,Flight" data-id="Flight-WP" data-icon="wordpress" data-label="Flight"><i class="fa fa-wordpress dropdown-icon"></i> Flight  <span style="background:#0074a2">WordPress</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/flight-wp.jpg"></li>
+
+<li class="button_a"><a href="#" rel="https://demo.themesuite.com/flight/,https://themeforest.net/item/flight-responsive-fullscreen-background-template/3053377?ref=themesuite,Flight" data-id="Flight" data-icon="html5" data-label="Flight"><i class="fa fa-html5 dropdown-icon"></i> Flight  <span style="background:#f16528">HTML5</span></a><img alt="" class="preview" src="https://demo.themesuite.com/images/previews/flight.jpg"></li>
+
+          </ul>
+        </li>
+      </ul>
+      <div class="responsive">
+        <a href="#" class="desktop active" title="View Desktop Version"></a>
+        <a href="#" class="tabletlandscape" title="View Tablet Landscape (1024x768)"></a>
+        <a href="#" class="tabletportrait" title="View Tablet Portrait (768x1024)"></a>
+        <a href="#" class="mobilelandscape" title="View Mobile Landscape (480x320)"></a>
+        <a href="#" class="mobileportrait" title="View Mobile Portrait (320x480)"></a>
+      </div>
+
+      <ul class="links">
+        <li class="purchase" rel="https://themeforest.net/item/automotive-car-dealership-business-wordpress-theme/9210971?ref=themesuite">
+          <a href="https://themeforest.net/item/automotive-car-dealership-business-wordpress-theme/9210971?ref=themesuite" target="_blank"><i
+              class="fa fa-shopping-cart button_icon"></i> Purchase</a>
+        </li>
+        <li class="close" rel="https://demo.themesuite.com/automotive-wp/">
+          <a href="https://demo.themesuite.com/automotive-wp/"><i class="fa fa-times button_icon"></i> Close</a>
+        </li>
+      </ul>
+
+      <div class="option_link">
+        <div class="blue_button animated"><i class="fa fa-cog options"></i> <span>Theme Options</span></div>
+
+        <div class="option_menu">
+          <img src="images/load.gif" class="loading_image" alt="loading"/>
+        </div>
+      </div>
+    </div>
+  </div>
+  <iframe id="iframe" src="https://demo.themesuite.com/automotive-wp/"></iframe>
+
+  <div id="loading_splash_screen">
+    <div class="loader">
+      <div class="color_1"></div>
+      <div class="color_2"></div>
+      <div class="color_3"></div>
+      <div class="color_4"></div>
+    </div>
+  </div>
+  </body>
+  </html>
---
+++ Resources/templates/front/side_nav.php
@@ -0,0 +1,13 @@
+
+<div class="col-md-3">
+    <p class="lead">Axis Electronics</p>
+    <div class="list-group">
+
+    	<?php 
+
+    		get_categories();
+
+    	 ?>
+
+    </div>
+</div>
